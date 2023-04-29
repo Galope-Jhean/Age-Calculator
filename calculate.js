f42aa37b410206ce125125cc1a232999698c28e2
Jhean <drakekhendrick@gmail.com>
2023-04-29 13:41:09 +0800

    fixed order of lies

--- calculate.js
+++ calculate.js
@@ -31,7 +31,7 @@ let getDate = (dayValue, monthValue, yearValue, currentYear, currentMonth, curre
         return [(currentDay += daysoftheMonths[currentMonth-1]) - dayValue, (currentMonth += 12) - monthValue, (currentYear -= 1) - yearValue];
     }
     if (dayValue > currentDay){
-        return [(currentDay += daysoftheMonths[currentMonth-1]) - dayValue, (currentMonth -= 1) - monthValue, currentYear - yearValue];
+        return [(currentDay += daysoftheMonths[currentMonth-1]) - dayValue, currentMonth - monthValue, currentYear - yearValue];
     }
     if(monthValue > currentMonth){
         return [currentDay - dayValue, ((currentMonth += 12) - monthValue), (currentYear -= 1) - yearValue];
